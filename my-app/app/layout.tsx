98ea059c302d50d1d72f1ec362b65058b7cfcddd
MelonConYogurt <alejopsornal@gmail.com>
2024-10-10 10:06:11 -0500

    commit frontend

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -44,12 +44,7 @@ export default function RootLayout({
         >
           <nav className="bg-gray-900 text-slate-50">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <div className="flex items-center justify-between h-16">
-                <div className="flex items-center">
-                  <Link href="/" className="flex-shrink-0">
-                    <div className="rounded-full bg-white h-8 w-8"></div>
-                  </Link>
-                </div>
+              <div className="flex items-center justify-center gap-5 h-16">
                 <div className="hidden md:block">
                   <div className="ml-10 flex items-baseline space-x-4 ">
                     <Link
